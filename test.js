c9c397f48b2e347892926435ed588e3a3c202c33
Wink Saville <wink@saville.com>
2017-04-10 12:57:50 -0700

    Test Expect.toThrow() for async functions.
    Expect.toThrow() with an async function fails the test when an
    error is thrown.
    ```
    $ yarn test
    yarn test v0.22.0
    $ tsc -p tsconfig.json
    $ alsatian --timeout 2000 test.js --tap | tap-fail-exit-one
    TAP version 13
    1..3
    # FIXTURE Test async toThrow
    testToThrow:+
    testToThrow:-
    ok 1 test alsatian toThrow
    testAsync:+ 1800
    asyncThrowError:+ delayMs=1800
    asyncThrowError:- delayMs=1800
    asyncThrowError.setTimeoutFunc:+ delayMs=1800 rejecting
    asyncThrowError.setTimeoutFunc:- delayMs=1800 rejected
    testAsync:- 1800
    ok 2 test alsatian async ( 1800 )
    testAsyncToThrow:+ 1750
    asyncThrowError:+ delayMs=1750
    asyncThrowError:- delayMs=1750
    not ok 3 test alsatian async toThrow ( 1750 )
     ---
       message: "Expected an error to be thrown but no errors were thown."
       severity: fail
       data:
         got: "error was not thrown."
         expect: "error to be thrown."
     ...
    asyncThrowError.setTimeoutFunc:+ delayMs=1750 rejecting
    asyncThrowError.setTimeoutFunc:- delayMs=1750 rejected
    (node:17962) UnhandledPromiseRejectionWarning: Unhandled promise rejection (rejection id: 2): Error: Timeout then reject
    (node:17962) DeprecationWarning: Unhandled promise rejections are deprecated. In the future, promise rejections that are not handled will terminate the Node.js process with a non-zero exit code.
    error Command failed with exit code 1.
    ```

---
+++ test.js
@@ -0,0 +1,94 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Test Expect.toThrow for async functions
+ */
+const alsatian_1 = require("alsatian");
+function debug(s) {
+    console.log(s);
+}
+let AsyncToThrowTests = class AsyncToThrowTests {
+    // Synchronous throw
+    throwError() {
+        throw new Error("an error");
+    }
+    testToThrow() {
+        debug("testToThrow:+");
+        alsatian_1.Expect(() => this.throwError()).toThrow();
+        debug("testToThrow:-");
+    }
+    // Asynchronous throw
+    asyncThrowError(delayMs) {
+        return new Promise((_, reject) => {
+            debug(`asyncThrowError:+ delayMs=${delayMs}`);
+            setTimeout(() => {
+                debug(`asyncThrowError.setTimeoutFunc:+ delayMs=${delayMs} rejecting`);
+                reject(new Error("Timeout then reject")),
+                    debug(`asyncThrowError.setTimeoutFunc:- delayMs=${delayMs} rejected`);
+            }, delayMs);
+            debug(`asyncThrowError:- delayMs=${delayMs}`);
+        });
+    }
+    testAsync(delayMs) {
+        return __awaiter(this, void 0, void 0, function* () {
+            debug(`testAsync:+ ${delayMs}`);
+            try {
+                yield this.asyncThrowError(delayMs);
+                alsatian_1.Expect("No error thrown, fail").not.toBeTruthy();
+            }
+            catch (err) {
+                alsatian_1.Expect(err).toBeTruthy();
+            }
+            debug(`testAsync:- ${delayMs}`);
+        });
+    }
+    testAsyncToThrow(delayMs) {
+        return __awaiter(this, void 0, void 0, function* () {
+            debug(`testAsyncToThrow:+ ${delayMs}`);
+            alsatian_1.Expect(() => this.asyncThrowError(delayMs)).toThrow();
+            debug(`testAsyncToThrow:- ${delayMs}`);
+        });
+    }
+};
+__decorate([
+    alsatian_1.Test("test alsatian toThrow"),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", []),
+    __metadata("design:returntype", void 0)
+], AsyncToThrowTests.prototype, "testToThrow", null);
+__decorate([
+    alsatian_1.TestCase(1800),
+    alsatian_1.AsyncTest("test alsatian async"),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Number]),
+    __metadata("design:returntype", Promise)
+], AsyncToThrowTests.prototype, "testAsync", null);
+__decorate([
+    alsatian_1.TestCase(1750),
+    alsatian_1.AsyncTest("test alsatian async toThrow"),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Number]),
+    __metadata("design:returntype", Promise)
+], AsyncToThrowTests.prototype, "testAsyncToThrow", null);
+AsyncToThrowTests = __decorate([
+    alsatian_1.TestFixture("Test async toThrow")
+], AsyncToThrowTests);
+exports.AsyncToThrowTests = AsyncToThrowTests;
+//# sourceMappingURL=test.js.map
---
+++ test.ts
@@ -0,0 +1,63 @@
+/**
+ * Test Expect.toThrow for async functions
+ */
+import {
+    AsyncTest,
+    Expect,
+    Test,
+    TestCase,
+    TestFixture,
+} from "alsatian";
+
+function debug(s: string) {
+    console.log(s);
+}
+
+@TestFixture("Test async toThrow")
+export class AsyncToThrowTests {
+    // Synchronous throw
+    public throwError() {
+        throw new Error("an error");
+    }
+
+    @Test("test alsatian toThrow")
+    public testToThrow() {
+        debug("testToThrow:+");
+        Expect(() => this.throwError()).toThrow();
+        debug("testToThrow:-");
+    }
+
+    // Asynchronous throw
+    private asyncThrowError(delayMs: number): Promise<void> {
+        return new Promise<void>((_, reject) => {
+            debug(`asyncThrowError:+ delayMs=${delayMs}`);
+            setTimeout(() => {
+                debug(`asyncThrowError.setTimeoutFunc:+ delayMs=${delayMs} rejecting`);
+                reject(new Error("Timeout then reject")),
+                debug(`asyncThrowError.setTimeoutFunc:- delayMs=${delayMs} rejected`);
+            }, delayMs);
+            debug(`asyncThrowError:- delayMs=${delayMs}`);
+        });
+    }
+
+    @TestCase(1800)
+    @AsyncTest("test alsatian async")
+    public async testAsync(delayMs: number) {
+        debug(`testAsync:+ ${delayMs}`);
+        try {
+            await this.asyncThrowError(delayMs);
+            Expect("No error thrown, fail").not.toBeTruthy();
+        } catch(err) {
+            Expect(err).toBeTruthy();
+        }
+        debug(`testAsync:- ${delayMs}`);
+    }
+
+    @TestCase(1750)
+    @AsyncTest("test alsatian async toThrow")
+    public async testAsyncToThrow(delayMs: number) {
+        debug(`testAsyncToThrow:+ ${delayMs}`);
+        Expect(() => this.asyncThrowError(delayMs)).toThrow();
+        debug(`testAsyncToThrow:- ${delayMs}`);
+    }
+}
